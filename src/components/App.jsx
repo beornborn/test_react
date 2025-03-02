ee124d590a5f73418e022d6c4a048ba1bcf5a98f
Oleg Gorbunov <oleg.gorbunov-contractor@procore.com>
2025-03-02 13:13:56 +0200

    add global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { AppContainer } from './App.style';
+import { GlobalStyle } from '../styles/global.style';
 
 export function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <AppContainer>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { App } from './components/App';
+import { GlobalStyle } from './styles/global.style';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
