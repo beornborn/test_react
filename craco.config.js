ddc02f25eed99cd0a20ebeb3c8269583a6b2bdbb
Oleg Gorbunov <oleg.gorbunov-contractor@procore.com>
2025-03-03 19:50:27 +0200

    update craco config

--- craco.config.js
+++ craco.config.js
@@ -6,4 +6,17 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  devServer: {
+    historyApiFallback: true,
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1', // Ensure Jest resolves '@/' correctly
+      },
+      moduleDirectories: ['src', 'node_modules'], // Prioritize 'src' over 'node_modules'
+      testEnvironment: 'jsdom',
+      setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    },
+  },
 };
